ead6d1328b23603c30a3e234b0dd6a45e76b0649
bogdan <b.pyanzin@mail.ru>
2023-11-02 23:56:06 +0700

    ver 3 old

---
+++ src/ru.nsu.mmf.syspro.project/main/java/ru/nsu/mmf/syspro/forth/operations/LogicOperation.java
@@ -0,0 +1,29 @@
+package ru.nsu.mmf.syspro.forth.operations;
+
+import ru.nsu.mmf.syspro.forth.Context;
+import ru.nsu.mmf.syspro.forth.exceptions.InterpreterException;
+
+public class LogicOperation implements Operation {
+    private final String command;
+    public LogicOperation(String command){
+        this.command=command;
+    }
+    @Override
+    public void apply(Context context) {
+        int l, r;
+        try {
+            r = context.stack.pop();
+            l = context.stack.pop();
+            context.stack.add(l);
+            context.stack.add(r);
+        } catch (EmptyStackException e) {
+            throw new InterpreterException("Not enough numbers on the stack");
+        }
+        boolean res = switch (command) {
+            case ">" -> l > r;
+            case "<" -> l < r;
+            default -> l == r;
+        };
+        context.stack.add(res ? 1 : 0);
+    }
+}
